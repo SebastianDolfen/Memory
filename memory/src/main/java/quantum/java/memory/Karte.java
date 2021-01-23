a7c60589ea62e68d899b75e5bb27b0534847d3da
SebastianDolfen <sebastiandolfen@rwth-aachen.de>
2021-01-23 23:05:21 +0100

    Spiellogik + win condition
    Und ein paar edge cases mit user input abgedeckt

--- memory/src/main/java/quantum/java/memory/Karte.java
+++ memory/src/main/java/quantum/java/memory/Karte.java
@@ -5,25 +5,29 @@ import java.awt.*;
 public class Karte {
     Image karte; 
     Image rückseite;
-    int size;
     int x;
     int y;
     boolean flipped;
+    int widthpos;
+    int heightpos;
+    String name;
     
-    public Karte(Image karte, int size,int x,int y,Image rückseite){
-        this.karte = karte;
-        this.size = size;
+    public Karte(Image karte, int size,int x,int y,Image rückseite,int widthpos,int heightpos,String name){
+        this.karte = karte.getScaledInstance(size, size, 4);
         this.x = x;
         this.y = y;
         this.flipped = false;
-        this.rückseite = rückseite;
+        this.widthpos = widthpos;
+        this.name = name;
+        this.heightpos = heightpos;
+        this.rückseite = rückseite.getScaledInstance(size, size, 4);
     }
     public void draw(Graphics g){
         if (flipped) {
-            g.drawImage(karte.getScaledInstance(size, size, 4),x,y, null);
+            g.drawImage(karte,x,y, null);
         }
         else{
-            g.drawImage(rückseite.getScaledInstance(size, size, 4),x,y, null);
+            g.drawImage(rückseite,x,y, null);
         }
     }
 }
--- memory/src/main/java/quantum/java/memory/MemoryState.java
+++ memory/src/main/java/quantum/java/memory/MemoryState.java
@@ -2,10 +2,10 @@ package quantum.java.memory;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 
 import quantum.java.memory.resources.Images;
 
@@ -14,8 +14,25 @@ public class MemoryState extends GameState{
 	List<Integer> widthpos;
 	List<Integer> heightpos;
 
+	int currentWidthPos = 0;
+	int currentHeightPos = 0;
+
 	Images images;
 	List<Karte> karten;
+	List<Karte> foundCards = new ArrayList<Karte>();
+
+	int activeCards = 0;
+	Karte card1;
+	Karte card2;
+	int wait = 0;
+	int errorwait = 0;
+
+	boolean error = false;
+	boolean win = false;
+
+	int restCards = 16;
+
+	int match = 0;
 
 	protected MemoryState(GameStateManager gsm) {
 		super(gsm);
@@ -65,7 +82,7 @@ public class MemoryState extends GameState{
 
 		for (int i = 0; i < widthpos.size(); i++) {
 			for (int j = 0; j < heightpos.size(); j++) {
-				karten.add(new Karte(images.images.get(names.get((4*(i))+(j))),Resize(300),(GamePanel.screenSize.width/2)+widthpos.get(i),(GamePanel.screenSize.height/2)+heightpos.get(j),images.images.get("Rückseite")));
+				karten.add(new Karte(images.images.get(names.get((4*(i))+(j))),Resize(300),(GamePanel.screenSize.width/2)+widthpos.get(i),(GamePanel.screenSize.height/2)+heightpos.get(j),images.images.get("Rückseite"),i,j,names.get((4*(i))+(j))));
 			}
 		}
 		
@@ -74,14 +91,55 @@ public class MemoryState extends GameState{
 
 	@Override
 	public void tick() {
-		
-		
+		if (match==2) {
+				
+
+			if (wait < 120) {
+				wait++;
+			}
+			else{
+				card1.flipped=false;
+				card2.flipped=false;
+				match = 0;
+				wait = 0;
+				activeCards=0;
+			}
+				
+		}
+		else if(match==1){
+			restCards = restCards-2;
+			foundCards.add(card1);
+			foundCards.add(card2);
+			match = 0;
+			activeCards=0;
+		}
+			
+		if (error) {
+			if (errorwait < 60) {
+				errorwait++;
+			}
+			else{
+				error = false;
+				errorwait = 0;
+			}
+		}
+		if (restCards==0) {
+			win = true;
+			gsm.states.pop();
+		}
 	}
 
 	@Override
 	public void draw(Graphics g) {
 		g.setColor(new Color(64, 64, 80));
-        g.fillRect(0, 0, GamePanel.screenSize.width, GamePanel.screenSize.height);
+		g.fillRect(0, 0, GamePanel.screenSize.width, GamePanel.screenSize.height);
+		if (error) {
+			g.setColor(new Color(191,97,106));
+		}
+		else{
+			g.setColor(green);
+		}
+		g.fillRect((((GamePanel.screenSize.width/2)+widthpos.get(currentWidthPos))-Resize(25)), (((GamePanel.screenSize.height/2)+heightpos.get(currentHeightPos))-Resize(25)), Resize(350), Resize(350));
         for (int i = 0; i < karten.size(); i++) {
 			karten.get(i).draw(g);
 		}
@@ -89,7 +147,72 @@ public class MemoryState extends GameState{
 
 	@Override
 	public void keyPressed(int k) {
-		
+		if (k==KeyEvent.VK_UP||k==KeyEvent.VK_W) {
+			if (currentHeightPos==0) {
+				currentHeightPos=3;
+			}
+			else{
+				currentHeightPos--;
+			}
+		}
+		else if(k==KeyEvent.VK_DOWN||k==KeyEvent.VK_S){
+			if (currentHeightPos==3) {
+				currentHeightPos=0;
+			}
+			else{
+				currentHeightPos++;
+			}
+		}
+		else if(k==KeyEvent.VK_LEFT||k==KeyEvent.VK_A){
+			if (currentWidthPos==0) {
+				currentWidthPos=3;
+			}
+			else{
+				currentWidthPos--;
+			}
+		}
+		else if(k==KeyEvent.VK_RIGHT||k==KeyEvent.VK_D){
+			if (currentWidthPos==3) {
+				currentWidthPos=0;
+			}
+			else{
+				currentWidthPos++;
+			}
+		}
+		else if(k==KeyEvent.VK_ENTER||k==KeyEvent.VK_SPACE){
+			for (int i = 0; i < karten.size(); i++) {
+				if (karten.get(i).heightpos==currentHeightPos&&karten.get(i).widthpos==currentWidthPos) {
+					if (karten.get(i).flipped==true) {
+						error = true;
+					}
+					else{
+						if (activeCards==0) {
+							karten.get(i).flipped=true;
+							card1 = karten.get(i);
+							activeCards++;
+						}
+						else if(activeCards==1){
+							karten.get(i).flipped=true;
+							activeCards++;
+							if (card1.name.replace("Text", "").replace("Siegel", "").equals(karten.get(i).name.replace("Text", "").replace("Siegel", ""))) {
+								match = 1;
+								card2 = karten.get(i);
+							}
+							else{
+								match = 2;
+								card2 = karten.get(i);
+							
+							}
+							
+						}
+						else{
+							error = true;
+						}
+					}
+					
+				}
+			}
+		}
 		
 	}
 
